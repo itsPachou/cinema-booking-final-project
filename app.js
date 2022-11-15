291cb7f66dda478e7c71caef49ccd51956697bcd
itsPachou <kacper.bziuk.99@gmail.com>
2022-11-15 20:53:27 +0000

    pug templating

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ import router from './router.js'
 
 const app = express()
 
+// Set pug as the templating engine
+app.set('view engine', 'pug')
+app.set(new URL('views', import.meta.url))
+
+app.use(express.static(new URL('public', import.meta.url).toString()))
+
+app.get('/', (req, res) => {
+    res.status(200).render('base')
+})
 app.use('/api/v1', router)
 
 export default app
