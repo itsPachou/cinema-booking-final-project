cc270add4b17be19648a0e3998f4f56d3596064e
itsPachou <kacper.bziuk.99@gmail.com>
2023-03-06 17:11:53 +0000

    model accurate dev data

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,21 +8,40 @@ const signToken = (id) =>
     })
 
 async function signup(req, res) {
-    const newUser = await User.create({
-        email: req.body.email,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        phoneNumber: req.body.phoneNumber,
-    })
+    try {
+        const newUser = await User.create({
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNumber: req.body.phoneNumber,
+        })
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser,
-        },
-    })
+        const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        })
+
+        const cookieOptions = {
+            expires: new Date(
+                Date.now() +
+                    process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+            ), // setting cookie expiration to 30 days in miliseconds
+            httpOnly: true, // make it so the browser can only get, store, and send back the cookie
+        }
+        if (process.env.NODE_ENV === 'production') cookieOptions.secure = true // only send cookie over HTTPS (only relevant once we go to production)
+        res.cookie('jwt', token, cookieOptions)
+
+        res.status(201).json({
+            status: 'success',
+            token,
+            data: {
+                user: newUser,
+            },
+        })
+    } catch (error) {
+        res.sendStatus(500)
+    }
 }
 
 async function login(req, res) {
@@ -66,4 +85,47 @@ async function login(req, res) {
     }
 }
 
-export { signup, login }
+async function protect(req, res, next) {
+    let token
+    let decoded
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1]
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt
+    }
+    if (!token) {
+        return res.redirect('/')
+    }
+
+    try {
+        decoded = await util.promisify(jwt.verify)(
+            token,
+            process.env.JWT_SECRET
+        )
+        const freshUser = await User.findById(decoded.id)
+        if (!freshUser) {
+            return res.redirect('/')
+        }
+        if (freshUser.changedPasswordAfter(decoded.iat)) {
+            return res.redirect('/')
+        }
+        req.user = freshUser
+    } catch (error) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+async function restrictTo(...roles) {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            res.sendStatus(403)
+        }
+        next()
+    }
+}
+
+export { signup, login, protect, restrictTo }
