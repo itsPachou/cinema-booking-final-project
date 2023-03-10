5cf7dfcda7f413e80a89397e1491351cc724ed04
itsPachou <kacper.bziuk.99@gmail.com>
2023-03-10 19:58:45 +0000

    add rooms to dev data import

--- dev-data/import-dev-data.js
+++ dev-data/import-dev-data.js
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv'
 import User from '../models/userModel.js'
 import Movie from '../models/cinemaModel.js'
 import Cinema from '../models/movieModel.js'
+import Room from '../models/roomModel.js'
 
 dotenv.config({ path: './config.env' })
 
@@ -22,21 +23,48 @@ mongoose.connect(DB).then(() => {
     console.log('DB connected.')
 })
 
-const users = JSON.parse(
+const { users } = JSON.parse(
     fs.readFileSync(new URL('./users.json', import.meta.url), 'utf-8')
 )
-const movies = JSON.parse(
+const { movies } = JSON.parse(
     fs.readFileSync(new URL('./movies.json', import.meta.url), 'utf-8')
 )
-const cinemas = JSON.parse(
+const { cinemas } = JSON.parse(
     fs.readFileSync(new URL('./cinemas.json', import.meta.url), 'utf-8')
 )
+const { rooms } = JSON.parse(
+    fs.readFileSync(new URL('./rooms.json', import.meta.url), 'utf-8')
+)
+
+const populateSeatPositionsInRooms = () => {
+    rooms.forEach((el) => {
+        const { width } = el.dimensions
+        const { length } = el.dimensions
+        for (let i = 0; i < length; i += 1) {
+            for (let j = 0; j < width; j += 1) {
+                el.seatPositions.push({ row: i, col: j })
+            }
+        }
+    })
+}
+
+const populateRoomsWithCinemas = async () => {
+    const cinema = await Cinema.find({
+        name: 'The Lumiere - Manchester',
+    }).exec()
+    rooms.forEach((element) => {
+        element.cinemaID = cinema._id.toString()
+    })
+}
 
 const importData = async () => {
     try {
         await User.create(users)
         await Movie.create(movies)
         await Cinema.create(cinemas)
+        await populateRoomsWithCinemas()
+        populateSeatPositionsInRooms()
+        await Room.create(rooms)
         console.log('Data imported.')
         process.exit()
     } catch (error) {
