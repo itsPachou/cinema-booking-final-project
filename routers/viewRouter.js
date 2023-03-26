e359d7e0be8d7085a3918add66047ea48ba03798
itsPachou <kacper.bziuk.99@gmail.com>
2023-03-26 16:25:08 +0100

    populate cinema list through pug

--- routers/viewRouter.js
+++ routers/viewRouter.js
@@ -1,16 +1,23 @@
 import express from 'express'
+import catchAsync from '../utils/catchAsync.js'
+import Cinema from '../models/cinemaModel.js'
 
 const viewRouter = express.Router()
 
 viewRouter.route('/').get((req, res) => {
     res.redirect(308, '/home')
 })
-viewRouter.route('/home').get((req, res) => {
-    res.set(
-        'Content-Security-Policy',
-        "default-src 'self';font-src fonts.gstatic.com;style-src 'self' 'unsafe-inline' fonts.googleapis.com"
-    )
-    res.status(200).render('home')
-})
+viewRouter.route('/home').get(
+    catchAsync(async (req, res, next) => {
+        const cinemas = await Cinema.find()
+        res.set(
+            'Content-Security-Policy',
+            "default-src 'self';font-src fonts.gstatic.com;style-src 'self' 'unsafe-inline' fonts.googleapis.com"
+        )
+        res.status(200).render('home', {
+            cinemas,
+        })
+    })
+)
 
 export default viewRouter
