9f097794c62faa5d746ae8a868849630e2403545
itsPachou <kacper.bziuk.99@gmail.com>
2023-04-17 15:09:24 +0100

    fixed typo

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -29,7 +29,7 @@ export default class APIFeatures {
 
     limitFields() {
         if (this.queryString.fields) {
-            const fields = this.queryString.fields.split(',').joing(' ')
+            const fields = this.queryString.fields.split(',').join(' ')
             this.query = this.query.select(fields)
         } else {
             this.query = this.query.select('-__v')
