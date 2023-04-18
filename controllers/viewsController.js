d9b84efaf22b6056ade10334ee320bdbe5ba331e
itsPachou <kacper.bziuk.99@gmail.com>
2023-04-18 15:45:00 +0100

    getCinemaPage handler with correct data and rename

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,6 +1,8 @@
 import catchAsync from '../utils/catchAsync.js'
 import Cinema from '../models/cinemaModel.js'
 import Screening from '../models/screeningModel.js'
+import Movie from '../models/movieModel.js'
+import groupByMovie from '../utils/groupByMovie.js'
 
 const getHomePage = catchAsync(async (req, res, next) => {
     const cinemas = await Cinema.find()
@@ -14,13 +16,21 @@ const getHomePage = catchAsync(async (req, res, next) => {
 })
 
 const getCinemaPage = catchAsync(async (req, res, next) => {
-    const screenings = await Screening.find({ cinemaID: req.params.slug })
+    // const screenings = await Screening.find({ cinemaID: req.params.slug })
+    const cinema = await Cinema.find({ slug: req.params.slug })
+    const screenings = await Screening.find({ cinemaID: cinema._id })
+    const movieIdArray = screenings.map((sc) => sc.movieID)
+    const movies = await Movie.find({ _id: movieIdArray })
+    const groupedScreenings = groupByMovie(screenings)
     res.set(
         'Content-Security-Policy',
         "default-src 'self';font-src fonts.gstatic.com;style-src 'self' 'unsafe-inline' fonts.googleapis.com"
     )
-    res.status(200).render('screenings', {
+    res.status(200).render('cinema', {
+        cinema,
         screenings,
+        movies,
+        groupedScreenings,
     })
 })
 
