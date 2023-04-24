3206326fc241561a16a241ca473e3091c89a4fa8
itsPachou <kacper.bziuk.99@gmail.com>
2023-04-24 21:52:37 +0100

    rooms CRUD

---
+++ routers/roomRouter.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import * as roomsController from '../controllers/roomsController.js'
+import * as authController from '../controllers/authController.js'
+
+const roomRouter = express.Router({ mergeParams: true })
+
+roomRouter
+    .route('/')
+    .get(roomsController.getAllRooms)
+    .post(
+        authController.protect,
+        authController.restrictTo('admin'),
+        roomsController.createRoom
+    )
+
+roomRouter
+    .route('/:id')
+    .get(roomsController.getRoom)
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin'),
+        roomsController.deleteRoom
+    )
+    .patch(
+        authController.protect,
+        authController.restrictTo('admin'),
+        roomsController.updateRoom
+    )
+
+export default roomRouter
