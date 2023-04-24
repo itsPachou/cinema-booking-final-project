b3ca0b215452edc1aead67546ec02ae9007bfa22
itsPachou <kacper.bziuk.99@gmail.com>
2023-04-24 15:59:49 +0100

    users CRUD and updateMe for users

---
+++ controllers/userController.js
@@ -0,0 +1,121 @@
+import User from '../models/userModel.js'
+import AppError from '../utils/appError.js'
+import catchAsync from '../utils/catchAsync.js'
+import APIFeatures from '../utils/apiFeatures.js'
+
+const filterUserObj = (obj, ...allowedFields) => {
+    const newObj = {}
+    Object.keys(obj).forEach((el) => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el]
+    })
+    return newObj
+}
+
+const updateMe = catchAsync(async (req, res, next) => {
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(
+            new AppError(
+                'This route is not for password updates. Use /updateMyPassword instead.',
+                400
+            )
+        )
+    }
+
+    const filteredUserObj = filterUserObj(
+        req.body,
+        'email',
+        'firstName',
+        'lastName',
+        'phoneNumber'
+    )
+
+    const updatedUser = await User.findByIdAndUpdate(
+        req.user._id,
+        filteredUserObj,
+        {
+            new: true,
+            runValidators: true,
+        }
+    )
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser,
+        },
+    })
+})
+
+const getAllUsers = catchAsync(async (req, res, next) => {
+    const features = new APIFeatures(User.find(), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate()
+    const users = await features.query
+
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users,
+        },
+    })
+})
+
+const getUser = catchAsync(async (req, res, next) => {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+        return next(new AppError('No user found with that ID', 404))
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user,
+        },
+    })
+})
+
+const createUser = catchAsync(async (req, res, next) => {
+    const newUser = await User.create(req.body)
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            user: newUser,
+        },
+    })
+})
+
+const updateUser = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    if (!user) {
+        return next(new AppError('No user found with that ID', 404))
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user,
+        },
+    })
+})
+
+const deleteUser = catchAsync(async (req, res, next) => {
+    const user = await User.findByIdAndDelete(req.params.id)
+
+    if (!user) {
+        return next(new AppError('No user found with that ID', 404))
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    })
+})
+
+export { updateMe, getAllUsers, getUser, updateUser, deleteUser, createUser }
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as authController from '../controllers/authController.js'
+import * as usersController from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
@@ -8,8 +9,36 @@ userRouter.route('/login').post(authController.login)
 userRouter.route('/logout').get(authController.logout)
 userRouter.route('/forgotPassword').post(authController.forgotPassword)
 userRouter.route('/resetPassword/:token').patch(authController.resetPassword)
+
 userRouter
     .route('/updateMyPassword')
     .patch(authController.protect, authController.updatePassword)
+
+userRouter
+    .route('/updateMe')
+    .patch(authController.protect, usersController.updateMe)
+
+userRouter
+    .route('/')
+    .get(usersController.getAllUsers)
+    .post(
+        authController.protect,
+        authController.restrictTo('admin'),
+        usersController.createUser
+    )
+
+userRouter
+    .route('/:id')
+    .get(usersController.getUser)
+    .delete(
+        authController.protect,
+        authController.restrictTo('admin'),
+        usersController.deleteUser
+    )
+    .patch(
+        authController.protect,
+        authController.restrictTo('admin'),
+        usersController.updateUser
+    )
 
 export default userRouter
