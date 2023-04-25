1b1f630129b9f592f69ab4ccaac9f51495858286
itsPachou <kacper.bziuk.99@gmail.com>
2023-04-25 12:54:19 +0100

    handler factory for update and delete one

--- controllers/cinemasController.js
+++ controllers/cinemasController.js
@@ -2,6 +2,7 @@ import Cinema from '../models/cinemaModel.js'
 import AppError from '../utils/appError.js'
 import catchAsync from '../utils/catchAsync.js'
 import APIFeatures from '../utils/apiFeatures.js'
+import * as factory from './handlerFactory.js'
 
 const getAllCinemas = catchAsync(async (req, res, next) => {
     const features = new APIFeatures(Cinema.find(), req.query)
@@ -44,35 +45,8 @@ const createCinema = catchAsync(async (req, res, next) => {
     })
 })
 
-const updateCinema = catchAsync(async (req, res, next) => {
-    const cinema = await Cinema.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-    })
+const updateCinema = factory.updateOne(Cinema)
 
-    if (!cinema) {
-        return next(new AppError('No cinema found with that ID', 404))
-    }
-
-    res.status(200).json({
-        status: 'success',
-        data: {
-            cinema,
-        },
-    })
-})
-
-const deleteCinema = catchAsync(async (req, res, next) => {
-    const cinema = await Cinema.findByIdAndDelete(req.params.id)
-
-    if (!cinema) {
-        return next(new AppError('No cinema found with that ID', 404))
-    }
-
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    })
-})
+const deleteCinema = factory.deleteOne(Cinema)
 
 export { getAllCinemas, createCinema, deleteCinema, getCinema, updateCinema }
